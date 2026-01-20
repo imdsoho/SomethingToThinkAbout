6211259a6ac070d68a5a98c9ba6212a2b7128941
imdsoho <imdsoho@gmail.com>
2026-01-20 14:50:24 +0900

    commit

---
+++ src/main/java/org/example/neighborNode/RawNode.java
@@ -0,0 +1,17 @@
+package org.example.neighborNode;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class RawNode {
+    private String start;
+    private String startNM;
+    private String end;
+    private String endNM;
+}
