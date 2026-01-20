1a4bbbb8636eb21ecb32c4e181462724eff9a389
imdsoho <imdsoho@gmail.com>
2026-01-20 14:48:44 +0900

    Node.code set commit

--- src/main/java/org/example/neighborNode/Node.java
+++ src/main/java/org/example/neighborNode/Node.java
@@ -1,16 +1,33 @@
 package org.example.neighborNode;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
+
+import java.util.Objects;
 
 @Getter
 @Setter
 @ToString
+@NoArgsConstructor
 @AllArgsConstructor
 public class Node {
-    //private String check;
     private String start;
     private String end;
+
+    /* Node.code 값만 사용하여 집합의 합집합 교집합을 구함 >> Main.v3 */
+//    private String code;
+//    private String name;
+//
+//    @Override
+//    public boolean equals(Object o) {
+//        if (this == o) return true;
+//        if (o == null || getClass() != o.getClass()) return false;
+//        Node item = (Node) o;
+//        //return Objects.equals(code, item.code) && Objects.equals(name, item.name);
+//        return Objects.equals(code, item.code);
+//    }
+//
+//    @Override
+//    public int hashCode() {
+//        return Objects.hash(code, name);
+//    }
 }
